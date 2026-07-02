14e4d0261966325166c1f6dae33c6cd959f10d9c
TT <tsegayetadesse558@gmail.com>
2026-07-02 03:49:28 -0700

    Delete Toast component from project
    Remove Toast component implementation.

--- Frontend/src/components/Toast.jsx
+++ Frontend/src/components/Toast.jsx
@@ -1,19 +1,1 @@
-import React from 'react';
-import { Snackbar, Alert } from '@mui/material';
 
-const Toast = ({ open, message, severity = 'success', onClose, autoHideDuration = 6000 }) => {
-  return (
-    <Snackbar
-      open={open}
-      autoHideDuration={autoHideDuration}
-      onClose={onClose}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-    >
-      <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
-        {message}
-      </Alert>
-    </Snackbar>
-  );
-};
-
-export default Toast;
